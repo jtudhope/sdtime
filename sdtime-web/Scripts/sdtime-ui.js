7d65e90d0bedf92cf80ae8cab0db996a47340ead
Ryan Gellis <rgellis90@gmail.com>
2012-12-13 00:22:45 -0500

    updates to structure and js for ui functionality

--- sdtime-web/Scripts/sdtime-ui.js
+++ sdtime-web/Scripts/sdtime-ui.js
@@ -1,1 +1,21 @@
-﻿
+﻿/* TOGGLE WEEK Click Event
+---------------------------------------- */
+jQuery(document).ready(function() {
+    jQuery('.toggle-week').click(function () {
+        jQuery('.toggle-day-selected').attr('class', 'toggle-day');
+        jQuery('.toggle-week').attr('class', 'toggle-week-selected');
+        jQuery('#content-table').animate({ marginLeft: '-940px' }, 250, function() {
+            
+            /* TOGGLE DAY Click Event (bound after week executed)
+            ---------------------------------------- */
+            jQuery('.toggle-day').click(function () {
+                jQuery('.toggle-week-selected').attr('class', 'toggle-week');
+                jQuery('.toggle-day').attr('class', 'toggle-day-selected');
+                jQuery('#content-table').animate({ marginLeft: '0' }, 250, function () {
+                    // any actions happening after animation?
+                });
+            });
+
+        });
+    });
+});
